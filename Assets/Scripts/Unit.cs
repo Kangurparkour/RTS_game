5b5db9c6227267a724889d7c94e7a68c54bd9c9c
Krzysztof Kowalik <kangurparkour@Gmail.com>
2021-01-25 00:05:05 +0100

    Added assets and navigation

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+    public Transform target;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+ 
+    void Update()
+    {
+        
+        if(target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+        Animate();
+    }
+
+    protected virtual void Animate()
+    {
+        var speedVecor = agent.velocity;
+        speedVecor.y = 0;
+        float speed = speedVecor.magnitude;
+        animator.SetFloat("Speed", speed);
+    }
+
+}
